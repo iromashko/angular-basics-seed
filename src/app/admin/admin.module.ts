6d6e2efbde99a50e8c71f7d2ba037e7136f18fee
Roman Dubyanskiy <r.dubyanskiy@atomyze.ru>
2022-10-13 00:13:35 +0300

    lesson 39

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
+import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
+import { DonutFormComponent } from './components/donut-form/donut-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DonutListComponent, DonutCardComponent],
-  imports: [CommonModule],
-  exports: [DonutListComponent],
+  declarations: [
+    DonutListComponent,
+    DonutCardComponent,
+    DonutSingleComponent,
+    DonutFormComponent,
+  ],
+  imports: [CommonModule, FormsModule],
+  exports: [DonutListComponent, DonutSingleComponent],
 })
 export class AdminModule {}
