92e7f2082997389ddfcbd39843d3f368e64316a9
Roman Dubyanskiy <r.dubyanskiy@atomyze.ru>
2022-10-12 19:50:29 +0300

    lesson 32

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -20,10 +20,11 @@ import { Donut } from '../../models/donut.model';
         <p class="donut-card-name">
           {{ donut.name }}
           <ng-container [ngSwitch]="donut.promo" ]>
-            <span *ngSwitchCase="'new'" class="donut-card-label">NEW</span>
-            <span *ngSwitchCase="'limited'" class="donut-card-label"
-              >LIMITED</span
-            >
+            <span  class="donut-card-label">
+              <ng-template [ngSwitchCase]="'new'"> NEW </ng-template>
+              <ng-template [ngSwitchCase]="'limited'"> LIMITED </ng-template>
+              <ng-template [ngSwitchDefault]> Nothing special... </ng-template>
+            </span>
           </ng-container>
         </p>
         <p class="donut-card-price">
